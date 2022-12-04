fea4f02d1b5aa18f84158c8ebcd3b74da4508c9a
Michael Chung <michaelchung@michaels-mbp.lan>
2022-12-03 21:44:14 -0500

    added init

---
+++ prwkv/__init__.py
@@ -0,0 +1,9 @@
+"""
+Production RWKV.
+RWKV - Receptance Weighted Key Value 
+This project aims to make RWKV Accessible to everyone using a Hugging Face like OOP interface, while staying close to the research.
+"""
+
+__version__ = "0.0.1"
+__author__ = 'Michael Chung (ArEnSc)'
+__credits__ = 'Blink DL, Em Ell e'
