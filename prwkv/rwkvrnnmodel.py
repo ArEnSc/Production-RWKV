7428196ef8f2664002cb4c92f2f45665805d3c80
Michael Chung <michaelchung@michaels-mbp.lan>
2023-01-15 11:30:46 -0500

    Full Mode added starts off at fp16

--- prwkv/rwkvrnnmodel.py
+++ prwkv/rwkvrnnmodel.py
@@ -49,7 +49,7 @@ class RWKV_RNN_Model():
                 eos_token_id:int = 0,
                 padd_token_id:int = 1,
                 device_type:str="cpu", # cpu or cuda
-                float_mode:str="fp32", # fp32 (good for cpu) // fp16 (might overflow) // bf16 (less accurate)
+                float_mode:str="fp16", # fp32 (good for cpu) // fp16 (might overflow) // bf16 (less accurate)
                 ):
         
         self.context_length = context_length
@@ -91,6 +91,10 @@ class RWKV_RNN_Model():
         self.repetition_set = set()
 
     def half(self,mode="fp16"):
+        self.args.FLOAT_MODE = mode
+        self.model = RWKV_RNN(self.args)
+        
+    def full(self,mode="fp32"):
         self.args.FLOAT_MODE = mode
         self.model = RWKV_RNN(self.args)
 
